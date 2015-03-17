b53c7b60e05c40c38c99c4c20171f393d6144fa9
onopko <me@onotakehiko.com>
2015-03-18 01:07:04 +0900

    Setup HTML5 Boilerplate

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Set this to the root of your project when deployed:
+http_path       = "/"
+css_dir         = "assets/css"
+sass_dir        = "assets/scss"
+images_dir      = "assets/images"
+javascripts_dir = "assets/js"
+fonts_dir       = "assets/fonts"
+httpimagespath  = "/assets/images"
+
+output_style    = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = false
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+preferred_syntax = :scss
+
+
+sass_options = { :debug_info => true }
